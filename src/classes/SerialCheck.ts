eeb8e2214488d420f28bbcc0f455e6ea85791266
Yoru <me@rlato.icu>
2022-10-20 20:13:16 +0900

    chore: typo

--- src/classes/SerialCheck.ts
+++ src/classes/SerialCheck.ts
@@ -6,16 +6,16 @@ const prisma = new PrismaClient()
 export default class MiddleWare {
   public static async verify (req: Request, res: Response & { locals: { identity: User} }, next: NextFunction) {
     const { sn } = req.headers
-    if (sn !== '64ba59ff-98dc-4f9d-a485-8e209b9957b6') return res.status(401).send({ sucess: false, message: 'Auth failed (SN required)' })
-    if (!sn) return res.status(401).send({ sucess: false, message: 'Auth failed (SN required)' })
+    if (sn !== '64ba59ff-98dc-4f9d-a485-8e209b9957b6') return res.status(401).send({ success: false, message: 'Auth failed (SN required)' })
+    if (!sn) return res.status(401).send({ success: false, message: 'Auth failed (SN required)' })
     try {
       const speaker = await prisma.user.findUnique({ where: { serialNumber: String(sn) } })
-      if (!speaker) return res.status(401).send({ sucess: false, message: 'SerialNumber not matched' })
+      if (!speaker) return res.status(401).send({ success: false, message: 'SerialNumber not matched' })
       res.locals.identity = speaker
       next()
     } catch (err: any) {
       Logger.error('Prisma').put(err.stack).out()
-      return res.status(500).send({ sucess: false, message: 'Internal Server Error (database failed)' })
+      return res.status(500).send({ success: false, message: 'Internal Server Error (database failed)' })
     }
   }
 }
